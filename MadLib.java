706864b7392e4b7331391c3c205ffbe1aebff68a
Koki <kgoudy64@gmail.com>
2022-07-10 07:59:09 -0500

    Add files via upload

---
+++ MadLib.java
@@ -0,0 +1,13 @@
+public class MadLib {
+    public static void main(String[] args) {
+        System.out.println("Welcome to my Mad Libs game");
+
+        System.out.println("My _____ dog accidentally ____ed all over the new ____" );
+
+        String adjective = "silly";
+        String verb = "spill";
+        String noun = "rug";
+
+        System.out.println("My " + adjective + " dog accidentally "  + verb + "ed water all over the new " + noun);
+    }
+}
